0d33560986bd119c8fe696cd9a799e1789c965e7
Wesley <wes.beck21@outlook.com>
2020-11-13 14:59:41 -0800

    commit tree baca35218ba205890f3ad12722583da4b4efbbcf

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_names)# code an argument here
   # Your code here
+  dwarf_names.each.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  i = 0
+  all_calls = []
+  while i < calls.length 
+    calls.collect do |call|
+    all_calls << "#{call}!".capitalize
+    i += 1
+  end
+    return all_calls
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.detect{|food| cheese_types.any?("#{food}")}
+    
 end
+
+
+#{|food| cheese_types.any?("#{food}")}
